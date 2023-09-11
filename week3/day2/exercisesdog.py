d82bbd7b299afb1f859cb2916de0a39b1a1960d2
meirary <meirary@gmail.com>
2023-09-11 14:16:27 +0300

    11/9

---
+++ week3/day2/exercisesdog.py
@@ -0,0 +1,38 @@
+from exercisexp import Dog 
+import random
+
+class PetDog(Dog):
+    def __init__(self, name, age, weight):
+        super().__init__(name, age, weight)
+        self.trained = False
+
+    def train(self):
+        bark_output = super().bark()
+        self.trained = True
+        print(bark_output)
+        print(f"{self.name} is now trained!")
+
+    def play(self, *dog_names):
+        dog_names_str = ", ".join(dog_names)
+        print(f"{dog_names_str} all play together")
+
+    def do_a_trick(self):
+        if self.trained:
+            tricks = [
+                f"{self.name} does a barrel roll",
+                f"{self.name} stands on his back legs",
+                f"{self.name} shakes your hand",
+                f"{self.name} plays dead"
+            ]
+            random_trick = random.choice(tricks)
+            print(random_trick)
+        else:
+            print(f"{self.name} is not trained yet. Train the dog first.")
+
+# Example usage:
+if __name__ == "__main__":
+    pet_dog = PetDog("Buddy", 3, 25)
+
+    pet_dog.train()  # Trains the dog
+    pet_dog.play("Rex", "Max", "Luna")  # Dogs play together
+    pet_dog.do_a_trick()  # Performs a random trick if trained
---
+++ week3/day2/exercisexp.py
@@ -0,0 +1,82 @@
+# Exercises 1
+
+class Pets():
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Bengal(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Chartreux(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+    
+class Siamese(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+# Create instances of different cat breeds
+bengal_cat = Bengal("Bengal Cat", 3)
+chartreux_cat = Chartreux("Chartreux Cat", 4)
+siamese_cat = Siamese("Siamese Cat", 2)
+
+# Create a list of all cats
+all_cats = [bengal_cat, chartreux_cat, siamese_cat]
+
+# Create an instance of the Pets class with all the cats
+sara_pets = Pets(all_cats)
+
+# Take all the cats for a walk
+sara_pets.walk()
+
+# Exercises 2
+
+class Dog:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def bark(self):
+        return f"{self.name} is barking"
+
+    def run_speed(self):
+        return self.weight / (self.age * 10)
+
+    def fight(self, other_dog):
+        self_speed = self.run_speed() * self.weight
+        other_speed = other_dog.run_speed() * other_dog.weight
+
+        if self_speed > other_speed:
+            return f"{self.name} won the fight"
+        elif other_speed > self_speed:
+            return f"{other_dog.name} won the fight"
+        else:
+            return "It's a draw"
+
+dog1 = Dog("Rex", 3, 25)
+dog2 = Dog("Buddy", 4, 30)
+dog3 = Dog("Didi", 6, 23)
+
+print(dog1.bark()) 
+print(dog2.run_speed())  
+print(dog1.fight(dog3)) 
+
+# Exercises 3
+
+
